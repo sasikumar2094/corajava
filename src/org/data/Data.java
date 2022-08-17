0fac670bc433534c81828cf031a71d6c92c279b6
sasikumar2094 <sasikumar2094@gmail.com>
2022-08-17 18:48:53 +0530

    Ashik done his coding

--- src/org/data/Data.java
+++ src/org/data/Data.java
@@ -1,10 +1,13 @@
 package org.data;
 
 public class Data {
-	
+
 	private void system1() {
 		System.out.println("System no is 1");
 
 	}
+	private void system2() {
+		System.out.println("System no is 2");
 
+}
 }
